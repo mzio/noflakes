37287aef090408b902c420587f9cceab48993c48
Roshan Padaki <roshanpadaki@gmail.com>
2019-01-06 21:43:57 -0800

    More stuff

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-const express = require('express')
-const app = express()
-const port = 4000
+const express = require("express");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20");
+const app = express();
+const port = 4000;
 
-app.get('/', (req, res) => res.send('Fish!'))
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.listen(port)
+passport.use(new GoogleStrategy({}));
+app.get("/", (req, res) => res.send("Fish!"));
+
+app.listen(port);
