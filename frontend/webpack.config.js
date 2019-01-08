763597c60948a54a57c0f3a6cfe9c0823611b751
Michael Zhang <michael@michaels-mbp-5.attlocal.net>
2019-01-07 18:03:28 -0800

    Update bundle exports

---
+++ frontend/webpack.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+    moduel: {
+        rules: [
+            {
+                test: /\.(js | jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"]
+            }
+        ]
+    },
+    resolve: { extensions: ["*", ".js", ".jsx"] },
+    output: {
+        path: path.resolve(__dirname, "dist/"),
+        publicPath: "/dist/",
+        filename: "bundle.js"
+    },
+    devServer: {
+        contentBase: path.join(__dirname, "public/"),
+        port: 3000,
+        publicPath: "http://localhost:3000/dist/",
+        hotOnly: true
+    },
+    plugins: [new webpack.HotModuleReplacementPlugin()]
+}
