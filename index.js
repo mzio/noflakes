e4df088259c83ec2741484bccc0df580a44bf02c
Roshan Padaki <roshanpadaki@gmail.com>
2019-01-07 17:51:42 -0800

    Factored environment variables.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const server = require('./backend');
-const PORT = process.env.PORT || 4000;
+const server = require("./backend");
+const PORT = process.env.PORT;
 
 server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
