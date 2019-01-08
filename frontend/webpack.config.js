e7676a95b3dc1c512aa0841838605df6e4db92de
Michael Zhang <michael@michaels-mbp-5.attlocal.net>
2019-01-07 20:47:16 -0800

    Update webpack configs

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -2,14 +2,15 @@ const path = require("path");
 const webpack = require("webpack");
 
 module.exports = {
+    entry: "./src/index.js",
+    mode: "development",
     module: {
         rules: [
             {
-                test: /\.(js | jsx)$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader"
-                }
+                test: /\.(js|jsx)$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: "babel-loader",
+                options: { presets: ["@babel/env"] }
             },
             {
                 test: /\.css$/,
@@ -30,4 +31,6 @@ module.exports = {
         hotOnly: true
     },
     plugins: [new webpack.HotModuleReplacementPlugin()]
-}
+};
+
+
