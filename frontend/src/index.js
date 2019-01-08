de8dca668adfec9962b1467a4e2215808e5d9359
Michael Zhang <michael@michaels-mbp-5.attlocal.net>
2019-01-08 09:31:15 -0800

    Implement TestTable Component

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App.js";
+import TestTable from "./components/TestTable.js";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<TestTable />, document.getElementById("root"));
