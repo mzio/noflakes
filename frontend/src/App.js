e296906a5750e1979edfd9f6f75898c39c660193
Michael Zhang <michael@michaels-mbp-5.attlocal.net>
2019-01-08 19:55:57 -0800

    Add react-router-dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,45 +1,23 @@
 import React, { Component } from "react";
 import { hot } from "react-hot-loader";
-import "./App.css";
+import { Navbar, PageHeader } from "react-bootstrap";
 import ReactDOM from "react-dom";
-import { Provider, Box, Card, Subhead, Heading, Text, Button } from "rebass";
-import { Hero, CallToAction, ScrollDownIndicator } from "react-landing-page";
+import Home from "./components/Home";
+
+import "./App.css";
+
+// import { Button } from 'react-bootstrap';
+// import TestTable from "./components/TestTable.js";
+// import PactForm from "./components/PactForm.js";
 
 class App extends Component {
   render() {
     return (
-      <Provider>
-        <Hero
-          color="black"
-          bg="white"
-          backgroundImage="https://source.unsplash.com/jxaj-UrzQbc/1600x900"
-        >
-          <Heading>Name of your app</Heading>
-          <Subhead>a couple more words</Subhead>
-          {/* <CallToAction href="/getting-started" mt={3}>
-            Get Started
-          </CallToAction> */}
-          <ScrollDownIndicator />
-        </Hero>
-      </Provider>
+      <div className="App">
+        <Home />
+      </div>
     );
   }
 }
-// const App = props => (
-//   <Provider>
-//     <Hero
-//       color="black"
-//       bg="white"
-//       backgroundImage="https://source.unsplash.com/jxaj-UrzQbc/1600x900"
-//     >
-//       <Heading>Name of your app</Heading>
-//       <Subhead>a couple more words</Subhead>
-//       <CallToAction href="/getting-started" mt={3}>
-//         Get Started
-//       </CallToAction>
-//       <ScrollDownIndicator />
-//     </Hero>
-//   </Provider>
-// );
 
 export default hot(module)(App);
