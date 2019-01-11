38709a2bb1f84cc7ac11d9f5cd1dda8b1d93f243
Roshan Padaki <roshanpadaki@gmail.com>
2019-01-10 20:48:58 -0800

    really hcacky

--- frontend/src/components/PactAddUserForm.js
+++ frontend/src/components/PactAddUserForm.js
@@ -105,7 +105,7 @@ export default class PactAddUserForm extends React.Component {
             }
           });
       } else {
-        resolve(false);
+        resolve(true);
       }
     });
   }
