7a1777af052dd63e3e57812d80ed5ddff7707476
Roshan Padaki <roshanpadaki@gmail.com>
2019-01-11 00:26:19 -0800

    Better listing using bootstrap

--- frontend/src/components/PactViewer.js
+++ frontend/src/components/PactViewer.js
@@ -7,11 +7,18 @@ class PactViewer extends React.Component {
 
   render() {
     return (
-      <div>
-        <h2>{this.props.pact.name}</h2>
-        <div>{this.props.pact.description}</div>
-        <div>{this.props.pact.users.join(", ")}</div>
-      </div>
+      <a
+        href="#"
+        class="list-group-item list-group-item-action flex-column align-items-start"
+      >
+        <div class="d-flex w-100 justify-content-between">
+          <h5 class="mb-1">{this.props.pact.name}</h5>
+          console.log(this.props.pact.startDate)
+          <small>{this.props.pact.startDate}</small>
+        </div>
+        <p class="mb-1">{this.props.pact.description}</p>
+        <small>{this.props.pact.users.join(", ")}</small>
+      </a>
     );
   }
 }
