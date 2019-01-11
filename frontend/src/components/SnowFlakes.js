a49821a78d4ab40c18514014114920b799fdd4c1
Michael Zhang <michael@michaels-mbp-5.attlocal.net>
2019-01-11 11:25:56 -0800

    Fix snowflakes render

--- frontend/src/components/SnowFlakes.js
+++ frontend/src/components/SnowFlakes.js
@@ -3,48 +3,50 @@ import Particles from "react-particles-js";
 
 export default class SnowFlakes extends React.Component {
   render() {
-    <Particles
-      params={{
-        particles: {
-          number: {
-            value: 42,
-            density: {
-              enable: true
-            }
-          },
-          shape: {
-            type: "images",
-            images: [
-              {
-                src:
-                  "https://commons.wikimedia.org/wiki/File:Emojione_2744.svg",
-                height: 20,
-                width: 20
+    return (
+      <Particles
+        params={{
+          particles: {
+            number: {
+              value: 42,
+              density: {
+                enable: true
               }
-            ]
-          },
-          move: {
-            random: true,
-            speed: 1.5,
-            direction: "down",
-            out_mode: "out"
-          }
-        },
-        interactivity: {
-          events: {
-            onclick: {
-              enable: true,
-              mode: "repulse"
+            },
+            shape: {
+              type: "images",
+              images: [
+                {
+                  src:
+                    "https://commons.wikimedia.org/wiki/File:Emojione_2744.svg",
+                  height: 20,
+                  width: 20
+                }
+              ]
+            },
+            move: {
+              random: true,
+              speed: 1.5,
+              direction: "down",
+              out_mode: "out"
             }
           },
-          modes: {
-            repulse: {
-              distance: 100,
-              duration: 2
+          interactivity: {
+            events: {
+              onclick: {
+                enable: true,
+                mode: "repulse"
+              }
+            },
+            modes: {
+              repulse: {
+                distance: 100,
+                duration: 2
+              }
             }
           }
-        }
-      }}
-    />;
+        }}
+      />
+    );
   }
 }
