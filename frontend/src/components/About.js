f32a5493774135eb2abdc438f75c42bad2384c12
Michael Zhang <michael@michaels-mbp-5.attlocal.net>
2019-01-11 12:55:13 -0800

    Stuff added

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -1,11 +1,13 @@
 // Rendering of the About Page
-import React from "react"
+import React from "react";
 
 export default function About(props) {
-    return (
-        <h2>About</h2>
-        <p>Snowflakes is an app that prevents friends from flaking</p>
-        <h2>Contact</h2>
-        <p>don't try</p>
-    )
+  return (
+    <div>
+      <h2>About</h2>
+      <p>Snowflakes is an app that prevents friends from flaking</p>
+      <h2>Contact</h2>
+      <p>don't try</p>
+    </div>
+  );
 }
