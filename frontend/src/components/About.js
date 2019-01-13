e68386401c490653e4a30f513b29b2a2ac1bcf46
Michael Zhang <michael@michaels-mbp-5.attlocal.net>
2019-01-12 19:13:49 -0800

    Add snowfall

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -1,13 +1,25 @@
 // Rendering of the About Page
 import React from "react";
+import SnowStorm from "react-snowstorm";
+if (process.env.BROWSER) require("./CreatePact.css");
 
 export default function About(props) {
   return (
     <div>
-      <h2>About</h2>
-      <p>Snowflakes is an app that prevents friends from flaking</p>
-      <h2>Contact</h2>
-      <p>don't try</p>
+      <div>{/* <SnowStorm snowCharacter="❄️" /> */}</div>
+      <h2 className="hd3">About</h2>
+      <p>
+        Snowflakes is an app that tries to stop friends from flaking. It's the
+        brainchild of two ordinary blockmates and a Harvard dorm room who
+        finally decided they had had enough from their flaking friend.
+      </p>
+      <h2 className="hd3">Special thanks to:</h2>
+      <ul>
+        <li>
+          <a href="https://github.com/burakcan/react-snowstorm">burakcan</a>:
+          snowing effect.
+        </li>
+      </ul>
     </div>
   );
 }
--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import HomeDefault, { HomeSignIn, HomeProfile } from "./HomeStates.js";
 import Login from "./Login.js";
 if (process.env.BROWSER) require("./Home.css");
+
 import fetch from "isomorphic-fetch";
 
 import PactAddUserForm from "./PactAddUserForm"; // REACT COMPONENT DEV ZONE
