20262bcb7b197bbc8e65d29cf776a8363bf7444c
Roshan Padaki <roshanpadaki@gmail.com>
2019-01-12 19:49:33 -0800

    Fixes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import Main from "./components/Main";
 import { hot } from "react-hot-loader";
 import NavMenu from "./components/NavMenu/NavMenu";
-import SnowStorm from "react-snowstorm";
+// import SnowStorm from "react-snowstorm";
 
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fab } from "@fortawesome/free-brands-svg-icons";
